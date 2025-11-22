52efdef804a499926919ffc687f34702240f8378
Saurabh Sharma <saurabhshcs@gmail.com>
2025-11-22 00:09:25 +0000

    initial project scafolfing.

---
+++ src/main/java/com/saurabhshcs/adtech/microservices/design/patterns/MicroservicesDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.adtech.microservices.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/adtech/microservices/design/patterns/MicroservicesDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.adtech.microservices.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
